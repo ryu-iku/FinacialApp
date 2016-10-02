2105ddfa0f4ff09280f3e952266cb01b5eda7228
yu <yu@ryu-iku>
2016-10-03 00:27:39 +0900

    computing profit @my PC 1003

--- src/java/source/DailyActionProfitShow.java
+++ src/java/source/DailyActionProfitShow.java
@@ -6,6 +6,7 @@
 package source;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -14,15 +15,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.simple.JSONObject;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.ParseException;
-import org.json.simple.parser.JSONParser;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
-/**
- *
- * @author y.liu
- */
 public class DailyActionProfitShow extends HttpServlet {
 
     /**
@@ -37,34 +32,79 @@ public class DailyActionProfitShow extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try{
+            ArrayList<DailyActionProfitDTO> dap=DailyActionProfitDAO.getInstance().showAllDailyActionProfit();
+            
             int strategyId=1;
             String brandCode="1301";
             //get strategy
             StrategyDTO strategyDTO=DAO.getInstance().getStrategyById(strategyId);
-            JSONObject strategyJson=strategyDTO.getStrategyJson();
+            JSONArray strategyJson=strategyDTO.getStrategyJson().getJSONArray("content");
             
-            //get daily stock price
-            ArrayList<DailyStockPriceDTO> dailyStockPriceDTOArray=DAO.getInstance().getDailyStockPriceByBrandCode(brandCode);
+//            //get daily stock price
+            ArrayList<DailyStockPriceDTO> dailyStockPriceDTOArray=new ArrayList<DailyStockPriceDTO>();
+            dailyStockPriceDTOArray=DAO.getInstance().getDailyStockPriceByBrandCode(brandCode);
             
             //sort daily stock price array by date
-            //http://stackoverflow.com/questions/18441846/how-to-sort-an-arraylist-in-java
             Collections.sort(dailyStockPriceDTOArray, new Comparator<DailyStockPriceDTO>(){
                 @Override
-                public int compare(DailyStockPriceDTO dto2, DailyStockPriceDTO dto1){
+                public int compare(DailyStockPriceDTO dto1, DailyStockPriceDTO dto2){
                     return dto1.getDate().compareTo(dto2.getDate());
                 }
             });
             
-            //use strategy json and daily stock price to figure out profits
+            //use strategy json and daily stock price to figure out price_increase and min, max
+            JSONArray condition_hold=strategyJson.getJSONObject(0).getJSONArray("condition"),
+                    condition_sell=strategyJson.getJSONObject(0).getJSONArray("condition");
+            float price_increase=0,profit = 0,total_profit=0,
+                    min_hold=Float.parseFloat(condition_hold.getJSONObject(0).getString("min")),
+                    max_hold=Float.parseFloat(condition_hold.getJSONObject(0).getString("max")),
+                    min_sell=Float.parseFloat(condition_sell.getJSONObject(0).getString("min")),
+                    max_sell=Float.parseFloat(condition_sell.getJSONObject(0).getString("max"));
+            int price_before,price_after;
+            String action="sell";
             
-            
+            //roop start
+//            int index=1;
+            for(int index=1;index<dailyStockPriceDTOArray.size();index++){
+                price_before=dailyStockPriceDTOArray.get(index-1).getClosingPrice();
+                price_after=dailyStockPriceDTOArray.get(index).getClosingPrice();
+                price_increase=price_after/price_before-1;
+                
+                //record profit
+                if("hold".equals(action)){profit=price_increase; total_profit+=profit;}
+                DailyActionProfitDTO dailyActionProfitDTO=new DailyActionProfitDTO();
+                dailyActionProfitDTO.setDate(dailyStockPriceDTOArray.get(index).getDate());
+                dailyActionProfitDTO.setBrandCode("brandCode");
+                dailyActionProfitDTO.setStrategyId(strategyId);
+                dailyActionProfitDTO.setAction(action);
+                dailyActionProfitDTO.setProfit(profit);
+                DAO.getInstance().setDailyActionProfit(dailyActionProfitDTO);
+
+                //change status
+                if(price_increase>=min_sell && price_increase<=max_sell){action="sell";}
+                else if (price_increase>=min_hold && price_increase<=max_hold){action="hold";}
+            }
             //html code maker
+            PrintWriter out = response.getWriter();
             response.setContentType("text/html;charset=UTF-8");
-            ArrayList<DailyActionProfitDTO> result=new ArrayList<DailyActionProfitDTO>();
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet testSublet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet testSublet at " + request.getContextPath() + "</h1>");
+            out.println(strategyDTO.getStrategyJson());
+            out.println("<br><br>");
+            out.println(dailyStockPriceDTOArray.size());
+            out.println("<br><br>");
+            out.println(condition_sell);
+            out.println("</body>");
+            out.println("</html>");
             
-            result=DAO.getInstance().getDailyActionProfitByStrategyId(strategyId);
-            request.setAttribute("RESULT",result);
-            request.getRequestDispatcher("/daily_action_profit_show.jsp").forward(request, response);
+            
+//            request.setAttribute("RESULT",result);
+//            request.getRequestDispatcher("/daily_action_profit_show.jsp").forward(request, response);
 
         }catch(Exception e){
             request.setAttribute("error", e.getMessage());
